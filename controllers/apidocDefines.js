918151e2e6495c418f443bca33a16afc9d6917ff
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-01-07 18:29:48 +0400

    feature/1.0/2: added apidocDefines.js

---
+++ controllers/apidocDefines.js
@@ -0,0 +1,13 @@
+/**
+ * apiDefine elements definitions for documentation generation.
+ */
+
+/**
+ * @apiDefine Public Public
+ * This API is publicly available to everyone.
+ */
+
+/**
+ * @apiDefine Private Private
+ * This API needs an authorization token to be accessible.
+ */
