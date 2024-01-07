4d6b49804a05383f2dc429c882db8f0eea4f97a8
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-01-07 15:55:33 +0400

    feature/1.0/2: added documentation

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -2,6 +2,37 @@ import httpStatus from 'http-status-codes';
 
 import dbUtil from '../utils/db.util.js';
 
+/**
+ * @api {GET} /book/all Get all books
+ * @apiGroup Book
+ * @apiName BookGetAll
+ *
+ * @apiParam {Number} [p=1] Page number
+ * @apiParam {String="ratingDesc", "ratingAsc", "dateAsc", "dateDesc"} [order=ratingDesc] Order by
+ *
+ * @apiSuccess (Success (200)) {Number} id The book id
+ * @apiSuccess (Success (200)) {String} isbn The book isbn
+ * @apiSuccess (Success (200)) {String} title The book title
+ * @apiSuccess (Success (200)) {String} notes The book notes
+ * @apiSuccess (Success (200)) {String} author The book author
+ * @apiSuccess (Success (200)) {Date} date The book published date
+ * @apiSUccess (Success (200)) {Number} rating The book rating
+ *
+ * @apiSuccessExample {json} Success-Response
+ * [{
+ *    "id": 4,
+ *    "isbn": "978-1-5247-6316-9",
+ *    "title": "A Promised Land",
+ *    "notes": "In the stirring, highly anticipated first volume of his presidential memoirs",
+ *    "author": "Barack Obama",
+ *    "date": "2020-11-17",
+ *    "rating": 10
+ * }]
+ *
+ * @apiError (Error (404)) NOT_FOUND Cannot find books
+ *
+ * @apiPermission Public
+ */
 const getAllBooks = async (req, res) => {
   try {
     let offset = 0;
@@ -43,6 +74,36 @@ const getAllBooks = async (req, res) => {
   }
 };
 
+/**
+ * @api {GET} /book/:id Get a book
+ * @apiGroup Book
+ * @apiName BookGet
+ *
+ * @apiParam {Number} id The book id
+ *
+ * @apiSuccess (Success (200)) {Number} id The book id
+ * @apiSuccess (Success (200)) {String} isbn The book isbn
+ * @apiSuccess (Success (200)) {String} title The book title
+ * @apiSuccess (Success (200)) {String} notes The book notes
+ * @apiSuccess (Success (200)) {String} author The book author
+ * @apiSuccess (Success (200)) {Date} date The book published date
+ * @apiSUccess (Success (200)) {Number} rating The book rating
+ *
+ * @apiSuccessExample {json} Success-Response
+ * {
+ *    "id": 4,
+ *    "isbn": "978-1-5247-6316-9",
+ *    "title": "A Promised Land",
+ *    "notes": "In the stirring, highly anticipated first volume of his presidential memoirs",
+ *    "author": "Barack Obama",
+ *    "date": "2020-11-17",
+ *    "rating": 10
+ * }
+ *
+ * @apiError (Error (404)) NOT_FOUND Cannot find book
+ *
+ * @apiPermission Public
+ */
 const getBook = async (req, res) => {
   try {
     const { id } = req.params;
